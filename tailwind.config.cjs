81604bb7f539e11913b3a45fe83ae9f2540bc4c4
Mark Tripney <mark.tripney@gmail.com>
2022-11-29 00:11:09 +0000

    Add soc med icons; begin base styling.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        // plexSerif: ["IBM Plex Serif", "serif"],
+        // plexSans: ["IBM Plex Sans", "sans-serif"],
+        angieSans: ["angie-sans", "sans-serif"],
+        edita: ["edita", "serif"],
+        editaSmall: ["edita-small", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
